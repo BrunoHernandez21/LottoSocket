437cb0fcd25a854e58dc4762a6685851030314f6
BrunoHernandez21 <ichimar21@gmail.com>
2022-08-16 14:18:55 -0500

    new ruts

--- src/ruts/emit/emit_controller.js
+++ src/ruts/emit/emit_controller.js
@@ -3,9 +3,9 @@ const db = require('./../../db/db');
 var controller = require('./../../websock/websock_controller');
 const send_all = async (req, res) => {
     try {
-         await db.query("SELECT * FROM eventos_videos", function (error, results, _fields) {
+         await db.query("SELECT * FROM videos_estadisticas", function (error, results, _fields) {
             if (error) throw error;
-            controller.io.emit('videos',{items:results});
+            controller.io.emit('estadisticas',{items:results});
             res.json({"resp":"emitido correctamente"});
           });          
     } catch (error) {

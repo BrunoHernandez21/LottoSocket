dfa511a325cee5d66a3fb84d9cd2a57c7772dc84
BrunoHernandez21 <ichimar21@gmail.com>
2022-09-22 01:27:35 -0500

    timezone remove

--- src/db/db.js
+++ src/db/db.js
@@ -5,5 +5,6 @@ var conexion= mysql.createConnection({
     port: process.env.DB_PORT|| "",
     user : process.env.DB_USER|| "",
     password : process.env.DB_PASSWORD|| "",
+    timezone :"-00:00",
 });
 module.exports = conexion;

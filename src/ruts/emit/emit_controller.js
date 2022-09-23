0b6608f3c5e76208911c44b0a8132db5269e3661
BrunoHernandez21 <ichimar21@gmail.com>
2022-09-22 20:44:21 -0500

    order by

--- src/ruts/emit/emit_controller.js
+++ src/ruts/emit/emit_controller.js
@@ -3,7 +3,7 @@ const db = require('./../../db/db');
 var controller = require('./../../websock/websock_controller');
 const send_all = async (req, res) => {
     try {
-         await db.query("SELECT * FROM videos_estadisticas", function (error, results, _fields) {
+         await db.query("SELECT * FROM videos_estadisticas ORDER BY fecha", function (error, results, _fields) {
             if (error) throw error;
             controller.io.emit('estadisticas',{items:results});
             res.json({"resp":"emitido correctamente"});
